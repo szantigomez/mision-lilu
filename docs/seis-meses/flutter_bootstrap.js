e8b8a6d8d8de12d4f54993208de932ebbbc629e8
szantigomez <santygomez5@gmail.com>
2026-09-20 18:12:49 -0300

    Reproducir musica al interactuar

--- docs/seis-meses/flutter_bootstrap.js
+++ docs/seis-meses/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1219093327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4200516311" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
